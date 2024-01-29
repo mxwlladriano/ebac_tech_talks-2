a213a7877b7cc947cca5c622dcc0fcea68aac661
mxwlladriano <mxwlladriano@gmail.com>
2024-01-29 20:48:43 -0300

    criando data do evento

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,6 @@
 AOS.init();
 
-const dataDoEvento = new Date("dec 12, 2025 19:00:00");
+const dataDoEvento = new Date("dec 12, 2026 19:00:00");
 const timeStampDoEvento = dataDoEvento.getTime();
 
 const contaAsHoras = setInterval(function() {
